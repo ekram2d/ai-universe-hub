be87cdfbc7b3aa0981d1e092fe1341c1ea653cca
Ekram Hossain <ekramhossain117@gmail.com>
2023-03-03 16:02:52 +0600

    model data added by dynamically

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const fetche_data = (len) => {
 
 
-      const spin=document.getElementById("spin");
+      const spin = document.getElementById("spin");
       spin.classList.remove("hidden");
       fetch("https://openapi.programming-hero.com/api/ai/tools")
             .then(res => res.json())
@@ -9,17 +9,14 @@ const fetche_data = (len) => {
                   //console.log(data)
                   spin.classList.add("hidden");
 
-            if(len === 0) {
-                  GetData(data.data.tools, );
+                  if (len === 0) {
+                        GetData(data.data.tools,);
 
-            }else{
-                  GetData(data.data.tools, len);
-            }
-                 
-                  // data.data.tools[0].slice(0,7).forEach(element => {
-                  //       console.log(element)
-                  // });
-                  // console.log(data.data.tools[0]);
+                  } else {
+                        GetData(data.data.tools, len);
+                  }
+
+                  
             })
 }
 
@@ -28,12 +25,12 @@ const fetche_data = (len) => {
 const GetData = (data, len) => {
       //console.log(data)
       const card = document.getElementById("card1");
-     // console.log(data[0].id)
+      // console.log(data[0].id)
       card.innerHTML = '';
 
       data.slice(0, len).forEach(element => {
             let j = 1;
-           // console.log(typeof element.id)
+            // console.log(typeof element.id)
             const ol = document.createElement("ol");
 
             element.features.forEach(value => {
@@ -45,8 +42,7 @@ const GetData = (data, len) => {
                   j++;
             });
 
-            //console.log(ol);
-            //console.log(element.image[0]);
+           
 
             let div = document.createElement('div');
             div.className += "card card-compact w-96 bg-base-100 shadow-xl m-3 p-4";
@@ -60,7 +56,7 @@ const GetData = (data, len) => {
     </figure>
     <h2 class="card-title">Features</h2>
     </div>`
-          
+
 
 
 
@@ -72,7 +68,7 @@ const GetData = (data, len) => {
             <div class=" flex justify-between mt-4">
                             <div>
                                     
-            <h2 class="mt-3 font-bold">${element.name }</h2>
+            <h2 class="mt-3 font-bold">${element.name}</h2>
       <div class="flex gap-1">
             <img class="w-[5%]" src=${"image/month.png"}>
             <h2>${element.published_in}</h2>
@@ -88,57 +84,144 @@ const GetData = (data, len) => {
             card.appendChild(div);
 
 
-           // console.log(div);
-
-
-            // console.log(element[i])
-
 
       });
 
 
 
-      
+
 
 }
-const details=(id)=>{
+const details = (id) => {
 
       const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
 
       fetch(URL)
-      .then(res=>res.json())
-      .then(data=>{
-            showMoreDetails(data.data);
-      })
+            .then(res => res.json())
+            .then(data => {
+                  showMoreDetails(data.data);
+            })
 }
 
 
-const showMoreDetails=(data)=>{
+const showMoreDetails = (data) => {
       console.log(data)
-for(let i in data) {
-     // console.log(i)
+
+      let { accuracy, description, features, image_link, input_output_examples, integrations
+            , logo, pricing, tool_name, use_cases, } = data;
+      const modalHead = document.getElementById("modal-head");
+      console.log();
+      modalHead.innerHTML = `${description}`;
+
+      const modalimg = document.getElementById("modal-img");
+      modalimg.src = image_link[0];
+
+      const FeaturesUl = document.getElementById("FeaturesUl");
+      const IntegrationsUL = document.getElementById("IntegrationsUL");
+      const modelImgDiv = document.getElementById("model-img-div");
+      modelImgDiv.innerHTML = '';
+
+      if (input_output_examples === null) {
+            modelImgDiv.innerHTML += `<p class="text-center font-bold">Can You give any example</p>`;
+            modelImgDiv.innerHTML += `<p class="text-center">Not Not Yet! Take a break!!!</p>`;
+      }
+      else {
+
+            modelImgDiv.innerHTML += `<p class="text-center font-bold">${input_output_examples[0].input}</p>`;
+            modelImgDiv.innerHTML += `<p class="text-center">${input_output_examples[0].output}</p>`;
+      }
+      // console.log(input_output_examples[0])
+
+      const modaldiv = document.getElementById("modal-div");
+      modaldiv.innerHTML = '';
+      // console.log(modaldiv);
+      if (pricing === null) {
+
+
+            modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Basic</div>`;
+
+            modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Pro</div>`;
+
+            modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Enterprice</div>`;
+
+            // <div class="border-2  m-1 shadow-lg">sga</div>
+
+
+
+
+      }
+      else {
+            for (let i in pricing) {
+                  if (pricing[i].price) {
+                        modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">${pricing[i].price}</div>`
+
+                        // <div class="border-2  m-1 shadow-lg">sga</div>
+
+                        // console.log(pricing[i].price);
+                  }
+                  else {
+                        if (i === 0) {
+                              modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Basic</div>`;
+                        }
+
+                        else if (i === 1) {
+                              modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Pro</div>`;
+
+                        }
+                        else {
+                              modaldiv.innerHTML += `<div class="border-2  m-1 shadow-lg">Free of <br>Cost/<br>Enterprice</div>`;
+                        }
+                  }
+            }
+
+
+      }
+      IntegrationsUL.innerHTML = "";
+      FeaturesUl.innerHTML = "";
+      if (integrations === null) {
+            // IntegrationsUL.innerHTML.remove.classList("list-disc");
+            IntegrationsUL.innerHTML = `<p>No Data Found</p>`
+
+      }
+      else {
+
+
+            integrations.forEach((value) => {
+
+                  const li = document.createElement("li");
+                  li.innerHTML = `<p>${value}</p>`
+                  IntegrationsUL.appendChild(li);
+                  //console.log(value);
+
+            }
+            )
+      }
+      //console.log(features)
+      if (features === null) {
+            FeaturesUl.innerHTML = `<p>No Data Found</p>`
+      }
+      else {
+            for (let i in features) {
+
+                  const li = document.createElement("li");
+                  li.innerHTML = `<p>${features[i].feature_name}</p>`
+                  FeaturesUl.appendChild(li);
+
+            }
+      }
+
 
 
 }
-//(20 > 15) ? "Yes" : "No";
-let {accuracy,description, features, image_link,input_output_examples,integrations
-,logo,pricing,tool_name,use_cases,} = data;
 
-console.log(description);
 
-}
 
-const showMore_fetche_data= () =>{
-      
+function showMore() {
+      // console.log('yes');
+
+
+      fetche_data(0);
 
 }
 
-function showMore(){
-    // console.log('yes');
-
-    
-     fetche_data(0);
-
-}
-
-fetche_data(1);
+fetche_data(6);
