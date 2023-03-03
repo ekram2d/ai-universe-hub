e06c72b9ebe1554193768fae9e5b21a817fc8619
Ekram Hossain <ekramhossain117@gmail.com>
2023-03-03 09:29:37 +0600

    card are created by dynamically and slice by 6

--- api.js
+++ api.js
@@ -23,7 +23,25 @@ const GetData = (data, len) => {
       console.log(data[0].features)
       card.innerHTML = '';
 
-   
+      data.slice(0, len).forEach(element => {
+           
+            const cardbody = document.getElementById('card-body');
+            div.appendChild(ol)
+            div.innerHTML += `<hr>
+            
+            <h2 class="m-2">${element.name }</h2>
+            `
+            card.appendChild(div);
+
+
+            console.log(div);
+
+
+            // console.log(element[i])
+
+
+      });
+
 
 
       
